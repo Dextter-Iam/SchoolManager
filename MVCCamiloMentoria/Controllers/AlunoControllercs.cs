a5151b2ad9bb1315a0fe99ef40587aa1e64d7de0
camilo.eduardo <camilo.eduardo@seveninovacoes.com.br>
2025-04-03 12:00:20 -0300

    Controllers - pt 1

---
+++ MVCCamiloMentoria/Controllers/AlunoControllercs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCamiloMentoria.Controllers
+{
+    public class AlunoControllercs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
