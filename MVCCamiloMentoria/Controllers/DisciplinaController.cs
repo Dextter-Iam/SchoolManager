f4d7706811cf153d0e812ff0a19727989afcac52
camilo.eduardo <camilo.eduardo@seveninovacoes.com.br>
2025-04-03 12:14:36 -0300

    sync - add Controllers PT2

---
+++ MVCCamiloMentoria/Controllers/DisciplinaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCamiloMentoria.Controllers
+{
+    public class DisciplinaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCCamiloMentoria/Controllers/ResponsavelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCamiloMentoria.Controllers
+{
+    public class ResponsavelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
