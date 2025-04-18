d5270a6a9ee2dbbb0bdeb1e47fb72731b258761d
camilo.eduardo <camilo.eduardo@seveninovacoes.com.br>
2025-04-18 17:53:36 -0300

    a

--- MVCCamiloMentoria/ViewModels/AulaViewModel.cs
+++ MVCCamiloMentoria/ViewModels/AulaViewModel.cs
@@ -1,6 +1,6 @@
-﻿using MVCCamiloMentoria.Models;
-using System.ComponentModel.DataAnnotations.Schema;
-namespace MVCCamiloMentoria.ViewModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCCamiloMentoria.Models
 {
     [NotMapped]
     public class AulaViewModel
@@ -12,11 +12,11 @@ namespace MVCCamiloMentoria.ViewModels
         public int ProfessorId { get; set; }
         public Professor? Professor { get; set; }
         public int TurmaId { get; set; }
-        public Turma? Turma { get; set; } 
+        public Turma? Turma { get; set; }
         public int DisciplinaId { get; set; }
-        public Disciplina? Disciplina { get; set; } 
+        public Disciplina? Disciplina { get; set; }
         public bool ConfirmacaoPresenca { get; set; }
-        public List<Aluno>? AlunosPresentes { get; set; } 
+        public List<Aluno>? AlunosPresentes { get; set; }
 
     }
 }
