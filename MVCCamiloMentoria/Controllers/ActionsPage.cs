1ef3317fc269364c64800d80a3ad013c39b76557
camilo.eduardo <camilo.eduardo@seveninovacoes.com.br>
2025-04-23 11:33:35 -0300

    sync

---
+++ MVCCamiloMentoria/Controllers/ActionsPage.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCamiloMentoria.Controllers
+{
+    public class ActionsPage : Controller
+    {
+        // GET: ActionsPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
