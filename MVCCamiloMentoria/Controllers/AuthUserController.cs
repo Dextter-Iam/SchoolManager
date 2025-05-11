0d68e7e8a42d0d92be8c86f2e73921036b1b0971
camilo.eduardo <camilocontato19@outlook.com>
2025-05-11 12:37:21 -0300

    loginpage

---
+++ MVCCamiloMentoria/Controllers/AuthUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCamiloMentoria.Controllers
+{
+    public class AuthUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
